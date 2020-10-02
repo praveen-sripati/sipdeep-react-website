6cd8d10e1fb1a1ffe0adb54f1b602cb5e8377580
Praveen <pvnsripati@gmail.com>
2020-10-03 03:03:02 +0530

    feat: Add Info of SidDeep in About us

--- src/Sections/Main/About/index.js
+++ src/Sections/Main/About/index.js
@@ -13,31 +13,53 @@ export const About = () => {
           <div className="row content" data-aos="fade-up">
             <div className="col-lg-6">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                “M/s. Sipdeep Alkaline India Pvt Ltd.” is incorporated in 2019
+                is a premier bottler of alkaline & antioxident drinking &
+                multiple uses water with highest 10-ph balance and antioxident
+                1000. The company is focused on providing miraculous benefits of
+                water to humans & plants. To strive to be the world leader in
+                manufacturing & supplying alkaline & antioxident water in
+                thermal stainsteel jar, Tetra pack."
+              </p>
+              <p>
+                Our company’s mission is to provide 100% customer satisfaction
+                by providing healthy packaged alkaline & antioxident water.
+              </p>
+              <p>
+                Water that is more alkaline supposedly reduces acid in the
+                bloodstream and:
               </p>
               <ul>
                 <li>
-                  <i className="ri-check-double-line"></i> Ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat
+                  <i className="ri-check-double-line"></i> Improves metabolism
                 </li>
                 <li>
-                  <i className="ri-check-double-line"></i> Duis aute irure dolor in
-                  reprehenderit in voluptate velit
+                  <i className="ri-check-double-line"></i> Increases energy
                 </li>
                 <li>
-                  <i className="ri-check-double-line"></i> Ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat
+                  <i className="ri-check-double-line"></i> Slows aging
+                </li>
+                <li>
+                  <i className="ri-check-double-line"></i> Improves digestion
+                </li>
+                <li>
+                  <i className="ri-check-double-line"></i> Reduces bone loss
                 </li>
               </ul>
             </div>
             <div className="col-lg-6 pt-4 pt-lg-0">
               <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
+                The company is setting up a mineral water plant in thane, a
+                suburb of mumbai. One of the directors mr.aparajit chandrabhan
+                shukla on his visit to germany grabbed the idea and started to
+                research about alkaline & antioxident water and its health
+                benefits. After extensive search he could not procure any
+                antioxident water (product) anywhere. Thus the thought process
+                started to research on setting up a plant to manafacture
+                alkaline & antioxident water. This water filteration machine
+                plant is totally a inhouse designed. Various parts are being
+                manufactured in india, also spare parts are being imported from
+                china and other countries. The technical support is from japan.
               </p>
               <a href="#" className="btn-learn-more">
                 Learn More
