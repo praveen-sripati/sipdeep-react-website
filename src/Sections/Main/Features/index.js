67ad8259a4f87739127fa4c6004cf46654bd2ef6
Praveen <pvnsripati@gmail.com>
2020-10-03 03:04:18 +0530

    feat: Add Four different features of SipDeep

--- src/Sections/Main/Features/index.js
+++ src/Sections/Main/Features/index.js
@@ -7,27 +7,49 @@ export const Features = () => {
         <div className="container">
           <ul className="nav nav-tabs row d-flex">
             <li className="nav-item col-3" data-aos="zoom-in">
-              <a className="nav-link active show" data-toggle="tab" href="#tab-1">
+              <a
+                className="nav-link active show"
+                data-toggle="tab"
+                href="#tab-1"
+              >
                 <i className="ri-gps-line"></i>
-                <h4 className="d-none d-lg-block">Modi sit est dela pireda nest</h4>
+                <h4 className="d-none d-lg-block">
+                  Alkaline & Antioxident Water
+                </h4>
               </a>
             </li>
-            <li className="nav-item col-3" data-aos="zoom-in" data-aos-delay="100">
+            <li
+              className="nav-item col-3"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
               <a className="nav-link" data-toggle="tab" href="#tab-2">
                 <i className="ri-body-scan-line"></i>
-                <h4 className="d-none d-lg-block">Unde praesenti mara setra le</h4>
+                <h4 className="d-none d-lg-block">
+                  Types of Alkaline & Antioxidant Water
+                </h4>
               </a>
             </li>
-            <li className="nav-item col-3" data-aos="zoom-in" data-aos-delay="200">
+            <li
+              className="nav-item col-3"
+              data-aos="zoom-in"
+              data-aos-delay="200"
+            >
               <a className="nav-link" data-toggle="tab" href="#tab-3">
                 <i className="ri-sun-line"></i>
-                <h4 className="d-none d-lg-block">Pariatur explica nitro dela</h4>
+                <h4 className="d-none d-lg-block">
+                  Stages Of Our Water Filtration & Manufacturing
+                </h4>
               </a>
             </li>
-            <li className="nav-item col-3" data-aos="zoom-in" data-aos-delay="300">
+            <li
+              className="nav-item col-3"
+              data-aos="zoom-in"
+              data-aos-delay="300"
+            >
               <a className="nav-link" data-toggle="tab" href="#tab-4">
                 <i className="ri-store-line"></i>
-                <h4 className="d-none d-lg-block">Nostrum qui dile node</h4>
+                <h4 className="d-none d-lg-block">Taste of our Product</h4>
               </a>
             </li>
           </ul>
@@ -36,37 +58,28 @@ export const Features = () => {
             <div className="tab-pane active show" id="tab-1">
               <div className="row">
                 <div className="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                  <h3>
-                    Voluptatem dignissimos provident quasi corporis voluptates
-                    sit assumenda.
-                  </h3>
+                  <h3>Our Product Alkaline & Antioxident Water</h3>
                   <p className="font-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    "Our team has been studying alkaline water for more than a
+                    decade and saw an opportunity to create a better product
+                    that we all could feel good about drinking. So we brought
+                    together a unique group of professionals, scientists, and
+                    water engineers to create the highest quality, chemical free
+                    alkaline water around."
                   </p>
-                  <ul>
-                    <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Duis aute irure dolor
-                      in reprehenderit in voluptate velit.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate trideta storacalaperda
-                      mastiro dolore eu fugiat nulla pariatur.
-                    </li>
-                  </ul>
                   <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum
+                    Water is important to every cell, tissue, and organ in your
+                    body. The best way to stay hydrated Some people claim that
+                    alkaline water is better than anything you could get from
+                    the tap. But the science isn't there to back it up. Water is
+                    a combination of hydrogen and oxygen. That's why you call it
+                    H2O. The amount of hydrogen ionic in water is measured as
+                    pH. Alkaline water has less hydrogen -- or a higher pH --
+                    than regular water. Water’s pH level ranges from 0 to 14,
+                    Alkaline water enthusiasts claim that its increased hydrogen
+                    provides greater hydration than regular water, especially
+                    after a hard workout. They blame plain water's low pH for
+                    all sorts of health problems, from osteoporosis to cancer.
                   </p>
                 </div>
                 <div className="col-lg-6 order-1 order-lg-2 text-center">
@@ -81,41 +94,27 @@ export const Features = () => {
             <div className="tab-pane" id="tab-2">
               <div className="row">
                 <div className="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                  <h3>
-                    Neque exercitationem debitis soluta quos debitis quo
-                    mollitia officia est
-                  </h3>
+                  <h3>4 Types of Alkaline & Antioxidant Water</h3>
                   <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum
-                  </p>
-                  <p className="font-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    So basically we are introducing 4 types of alkaline &
+                    antioxidant water
                   </p>
                   <ul>
                     <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat.
+                      <i className="ri-check-double-line"></i> 10 pH level –
+                      it’s a drinking water
                     </li>
                     <li>
-                      <i className="ri-check-double-line"></i> Duis aute irure dolor
-                      in reprehenderit in voluptate velit.
+                      <i className="ri-check-double-line"></i> 7 pH level- for
+                      kids
                     </li>
                     <li>
-                      <i className="ri-check-double-line"></i> Provident mollitia
-                      neque rerum asperiores dolores quos qui a. Ipsum neque
-                      dolor voluptate nisi sed.
+                      <i className="ri-check-double-line"></i> 4.5 pH level- for
+                      hair & face
                     </li>
                     <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate trideta storacalaperda
-                      mastiro dolore eu fugiat nulla pariatur.
+                      <i className="ri-check-double-line"></i> 2 pH level –
+                      sanitizer, cleanser, agricultural
                     </li>
                   </ul>
                 </div>
@@ -132,35 +131,59 @@ export const Features = () => {
               <div className="row">
                 <div className="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
                   <h3>
-                    Voluptatibus commodi ut accusamus ea repudiandae ut autem
-                    dolor ut assumenda
+                    7 Stages Of Our Alkaline Water Filtration & Manufacturing
+                    and its Process
                   </h3>
                   <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum
+                    <strong>Water chlorination:</strong>&nbsp; Water
+                    chlorination is the process of adding chlorine or chlorine
+                    compounds such as sodium hypochlorite to water. This method
+                    is used to kill certain bacteria and other microbes in tap
+                    water as chlorine is highly toxic. In particular,
+                    chlorination is used to prevent the spread of waterborne
+                    diseases such as cholera, dysentery, and typhoid. the raw
+                    water, after chlorination is required to be stored to
+                    achieve a detention time as per process.
                   </p>
-                  <ul>
-                    <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Duis aute irure dolor
-                      in reprehenderit in voluptate velit.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Provident mollitia
-                      neque rerum asperiores dolores quos qui a. Ipsum neque
-                      dolor voluptate nisi sed.
-                    </li>
-                  </ul>
-                  <p className="font-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                  <p>
+                    <strong>R/o Purification:</strong>&nbsp; Purification
+                    process that uses a partially permeable membrane to remove
+                    ions, unwanted molecules and larger particles from drinking
+                    water, RO system rejects all the micro-organisms like
+                    bacteria & virus.
+                  </p>
+                  <p>
+                    <strong>UV Disinfection:</strong>&nbsp; Ultra Violet System
+                    (UV) is a water purification method used to disinfect the
+                    bacteria from the water. The ultraviolet rays perforate the
+                    harmful pathogens in your home's water and kill the illness
+                    causing microorganisms by attacking their genetic core.
+                  </p>
+                  <p>
+                    <strong>Ionizer:</strong>&nbsp; Ionized water helps to
+                    improve health condition and maintain a healthy lifestyle.
+                    Ionized water helps fight and prevent disease like
+                    arthritis, skin disorders, overweight, diabetes and cancer.
+                    Alkaline water restores the body's pH balance, work as a
+                    powerful antioxidant and maintain hydration at the cellular
+                    level.
+                  </p>
+                  <p>
+                    <strong>Ozonation:</strong>&nbsp; Ozone is used in water
+                    treatment for its oxidative qualities. Ozone oxidizes both
+                    organic & inorganic substances. One of the fastest and most
+                    thorough disinfection oliotreatments available for water,
+                    ozone does not lead to the formation of trihalomethanes
+                    (THMs).
+                  </p>
+                  <p>
+                    <strong>Liquid add-on:</strong>&nbsp; Further liquid
+                    additives are added to increase the life span of water.
+                  </p>
+                  <p>
+                    <strong>Bottle rinser & filling:</strong>&nbsp; The bottles
+                    are fed into the machine which automatically rinses, fills
+                    and screws the cap on it.
                   </p>
                 </div>
                 <div className="col-lg-6 order-1 order-lg-2 text-center">
@@ -175,38 +198,17 @@ export const Features = () => {
             <div className="tab-pane" id="tab-4">
               <div className="row">
                 <div className="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                  <h3>
-                    Omnis fugiat ea explicabo sunt dolorum asperiores sequi
-                    inventore rerum
-                  </h3>
+                  <h3>How Does It Taste?</h3>
                   <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum
+                    Alkaline water may taste bitter or different from your tap
+                    water. This can also change the taste of anything you use it
+                    in like coffee ,tea, vegetables And food.
                   </p>
                   <p className="font-italic">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua.
                   </p>
-                  <ul>
-                    <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Duis aute irure dolor
-                      in reprehenderit in voluptate velit.
-                    </li>
-                    <li>
-                      <i className="ri-check-double-line"></i> Ullamco laboris nisi
-                      ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate trideta storacalaperda
-                      mastiro dolore eu fugiat nulla pariatur.
-                    </li>
-                  </ul>
                 </div>
                 <div className="col-lg-6 order-1 order-lg-2 text-center">
                   <img
